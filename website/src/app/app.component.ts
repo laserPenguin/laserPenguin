c1b7bdbdaef27dabeb0e57ac06c27c022f8616b9
roque92 <leroque.asrc@gmail.com>
2024-04-11 22:38:45 +0000

    Creation of navbar in process

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -9,5 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'website';
+  logoTitle = 'Laser Penguin';
+  
+  //List title
+  option1 = 'Inicio';
+  option2 = 'Lo mas vendido';
+  option3 = 'Categorias';
+  option4 = 'Acerca de';
 }
