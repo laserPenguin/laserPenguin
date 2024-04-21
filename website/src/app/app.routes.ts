470e3d2183c32f4ddc346e95fa3c2e1c2f0a00ef
Jose Roque <leroque.asrc@gmail.com>
2024-04-21 18:25:37 +0000

    404 page created

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { InicioComponent } from './views/inicio/inicio.component';
 import { MasVendidoComponent } from './views/mas-vendido/mas-vendido.component';
 import { CategoriasComponent } from './views/categorias/categorias.component';
 import { AcercaComponent } from './views/acerca/acerca.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: '', component: InicioComponent},
-    {path: 'masvendido', component: MasVendidoComponent},
-    {path: 'categorias', component: CategoriasComponent},
-    {path: 'acerca', component: AcercaComponent}
+    {path: 'Inicio', title: 'Inicio', component: InicioComponent},
+    {path: 'masvendido', title: 'Lo mas vendido', component: MasVendidoComponent},
+    {path: 'categorias', title: 'Categorias', component: CategoriasComponent},
+    {path: '', redirectTo: 'Inicio', pathMatch: 'full'},
+    {path: 'acerca', title: 'Acerca de', component: AcercaComponent},
+    {path: '**', component: PageNotFoundComponent}
     ];
--- website/src/app/components/main-content/main-content.component.ts
+++ website/src/app/components/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { MasVendidoComponent } from '../../views/mas-vendido/mas-vendido.compone
 import { CategoriasComponent } from '../../views/categorias/categorias.component';
 import { AcercaComponent } from '../../views/acerca/acerca.component';
 import { RouterOutlet } from '@angular/router';
+import { PageNotFoundComponent } from '../../views/page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent, MasVendidoComponent, CategoriasComponent, AcercaComponent],
+  imports: [RouterOutlet, InicioComponent, MasVendidoComponent, CategoriasComponent, AcercaComponent, PageNotFoundComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
