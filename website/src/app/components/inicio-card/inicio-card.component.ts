4e3d5ed872ed1edc6311c988b3718239beda3be5
Jose Roque <leroque.asrc@gmail.com>
2024-05-01 23:04:34 +0000

    Category Button component finished NgFor Directive added to Card and Categories components

--- website/src/app/components/inicio-card/inicio-card.component.ts
+++ website/src/app/components/inicio-card/inicio-card.component.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-card',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './inicio-card.component.html',
   styleUrl: './inicio-card.component.css',
 })
 export class InicioCardComponent {
-  cardText =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-    '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique,';
+  cardItems = [
+    {
+      imageURL: 'https://picsum.photos/200',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+    {
+      imageURL: 'https://picsum.photos/100',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+    {
+      imageURL: 'https://picsum.photos/150',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+    {
+      imageURL: 'https://picsum.photos/300',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+    {
+      imageURL: 'https://picsum.photos/350',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+    {
+      imageURL: 'https://picsum.photos/400',
+      context:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+    },
+  ];
 }
--- website/src/app/components/inicio-categorias/inicio-categorias.component.ts
+++ website/src/app/components/inicio-categorias/inicio-categorias.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-categorias',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './inicio-categorias.component.html',
-  styleUrl: './inicio-categorias.component.css'
+  styleUrl: './inicio-categorias.component.css',
 })
 export class InicioCategoriasComponent {
-
+  categoryNames = [
+    { name: 'Accesorios' },
+    { name: 'Navideño' },
+    { name: 'Cocina' },
+    { name: 'Gamer' },
+    { name: 'Anime' },
+  ];
+  customize = 'Personalizado';
 }
