573181d013dc34c14847c66f121cb0dc74c469b1
Jose Roque <leroque.asrc@gmail.com>
2024-05-10 21:08:48 +0000

    Pop Component desig

--- website/src/app/components/inicio-card/inicio-card.component.ts
+++ website/src/app/components/inicio-card/inicio-card.component.ts
@@ -12,43 +12,60 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class InicioCardComponent {
   modalTitle = 'Descripcion';
   modalOrderButton = 'Ordena Ahora';
+  modalPrice = Math.floor(Math.random() * 1000) / 10;
+
   cardItems = [
     {
+      id: 1,
       imageURL: 'https://picsum.photos/200',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
-      price: Math.floor(Math.random() * 100),
+      price: Math.floor(Math.random() * 1000) / 10,
     },
     {
+      id: 2,
       imageURL: 'https://picsum.photos/100',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+      price: Math.floor(Math.random() * 1000) / 10,
     },
     {
+      id: 3,
       imageURL: 'https://picsum.photos/150',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+      price: Math.floor(Math.random() * 1000) / 10,
     },
     {
+      id: 4,
       imageURL: 'https://picsum.photos/300',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+      price: Math.floor(Math.random() * 1000) / 10,
     },
     {
+      id: 5,
       imageURL: 'https://picsum.photos/350',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+      price: Math.floor(Math.random() * 1000) / 10,
     },
     {
+      id: 6,
       imageURL: 'https://picsum.photos/400',
       context:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
         '\nVestibulum accumsan magna quis ipsum blandit commodo. Morbi tristique',
+      price: Math.floor(Math.random() * 1000) / 10,
     },
   ];
+
+  getID(id: number) {
+    console.log(id);
+  }
 }
