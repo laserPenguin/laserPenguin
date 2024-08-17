ebe15e45285087fcb539b14301f54f0bad018876
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:16:21 -0600

    Update FILES .spec.ts

--- website/src/app/views/page-not-found/page-not-found.component.spec.ts
+++ website/src/app/views/page-not-found/page-not-found.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { PageNotFoundComponent } from './page-not-found.component';
-import {RouterTestingModule} from "@angular/router/testing";
 
 describe('PageNotFoundComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [PageNotFoundComponent],
-      providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
-      ],
+      imports: [RouterTestingModule, PageNotFoundComponent],
     }).compileComponents();
   });
 
