dc93f33811e4bcdc0339e945defd0ea7ad1c8aa8
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:07:17 -0600

    Update app.component.ts

--- website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
+++ website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InicioCategoriasComponent } from './inicio-categorias.component';
 
 describe('InicioCategoriasComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [InicioCategoriasComponent],
-      providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
-      ],
+      imports: [RouterTestingModule, InicioCategoriasComponent],
     }).compileComponents();
   });
 
--- website/src/app/components/navbar/navbar.component.spec.ts
+++ website/src/app/components/navbar/navbar.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [NavbarComponent],
-      providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
-      ],
+      imports: [RouterTestingModule, NavbarComponent],
     }).compileComponents();
   });
 
