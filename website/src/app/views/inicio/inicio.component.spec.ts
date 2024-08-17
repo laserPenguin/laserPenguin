1395b49e94ea4d819d639b9c34088a2d67eebb37
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:17:18 -0600

    Update FILES .spec.ts

--- website/src/app/views/inicio/inicio.component.spec.ts
+++ website/src/app/views/inicio/inicio.component.spec.ts
@@ -1,22 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { InicioComponent } from './inicio.component';
 
 describe('InicioComponent', () => {
-  let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InicioComponent],
+      imports: [RouterTestingModule, InicioComponent],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(InicioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(InicioComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
