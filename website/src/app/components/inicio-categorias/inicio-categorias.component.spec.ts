4612b98560a2b8e58df0b47a394c2390bfab44ee
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:08:52 -0600

    Update FILES .spec.ts

--- website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
+++ website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
@@ -1,15 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InicioCategoriasComponent } from './inicio-categorias.component';
-import {ActivatedRoute} from "@angular/router";
 
 describe('InicioCategoriasComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, InicioCategoriasComponent],
-      providers: [
-        {provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
-      ],
     }).compileComponents();
   });
 
