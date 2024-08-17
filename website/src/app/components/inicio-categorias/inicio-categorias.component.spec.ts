53437a85dd71daa8d4d2d2be5764aa69e458342c
roque92 <leroque.asrc@gmail.com>
2024-08-17 14:57:53 -0600

    Update app.component.ts

--- website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
+++ website/src/app/components/inicio-categorias/inicio-categorias.component.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { InicioCategoriasComponent } from './inicio-categorias.component';
 
 describe('InicioCategoriasComponent', () => {
-  let component: InicioCategoriasComponent;
-  let fixture: ComponentFixture<InicioCategoriasComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InicioCategoriasComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(InicioCategoriasComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [InicioCategoriasComponent, RouterTestingModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
+      ],
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(InicioCategoriasComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
