1172f183d01ad9ebaa7b4a3cb02ebef63c2d6aeb
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:09:07 -0600

    Update FILES .spec.ts

--- website/src/app/views/page-not-found/page-not-found.component.spec.ts
+++ website/src/app/views/page-not-found/page-not-found.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('PageNotFoundComponent', () => {
-  let component: PageNotFoundComponent;
-  let fixture: ComponentFixture<PageNotFoundComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageNotFoundComponent],
+      imports: [RouterTestingModule],
+      declarations: [PageNotFoundComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { snapshot: { params: {} } } }
+      ],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(PageNotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(PageNotFoundComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
