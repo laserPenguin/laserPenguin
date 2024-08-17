898f36c2a1d7a4518c0d5b3deb2958708c53c665
roque92 <leroque.asrc@gmail.com>
2024-08-17 15:25:18 -0600

    Update FILES .spec.ts

--- website/src/app/app.component.spec.ts
+++ website/src/app/app.component.spec.ts
@@ -1,35 +1,53 @@
-import { TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing'; // Importa RouterTestingModule
+import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {MainContentComponent} from "./components/main-content/main-content.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent], // Usa declarations en lugar de imports para componentes
-      imports: [RouterTestingModule], // Agrega RouterTestingModule si se usa en tu componente
-      providers: [
-        { provide: ActivatedRoute, useValue: {} } // Proporciona una instancia falsa de ActivatedRoute
+      declarations: [
+        AppComponent,
+        NavbarComponent,
+        MainContentComponent,
+        FooterComponent,
+        RouterOutlet
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'website' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('website');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain <app-navbar>', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('website');
+    expect(compiled.querySelector('app-navbar')).toBeTruthy();
+  });
+
+  it('should contain <app-main-content>', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-main-content')).toBeTruthy();
+  });
+
+  it('should contain <app-footer>', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-footer')).toBeTruthy();
+  });
+
+  it('should contain <router-outlet>', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
